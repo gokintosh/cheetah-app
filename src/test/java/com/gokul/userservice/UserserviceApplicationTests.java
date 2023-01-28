5d3eac3f88046bb6facda72dc25b8b17acfc4f31
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2023-01-28 15:11:03 +0100

    init project

---
+++ src/test/java/com/gokul/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
